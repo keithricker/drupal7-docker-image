c2464a8d073317631c8e89b4459d45282b63ef05
Keith Ricker <keithricker@hotmail.com>
2016-10-04 00:27:13 -0500

    Update kricker-d7-start.sh

--- config/kricker-d7-start.sh
+++ config/kricker-d7-start.sh
@@ -11,36 +11,6 @@ if [ "${PRIVATE_KEY_CONTENTS}" != "" ]; then
     sed -i 's/\\n/\
 /g' ~/.ssh/${PRIVATE_KEY_FILE}
     chmod 600  ~/.ssh/${PRIVATE_KEY_FILE}
-    sed -i \
-        -e 's/^#*\(PermitRootLogin\) .*/\1 yes/' \
-        -e 's/^#*\(PasswordAuthentication\) .*/\1 yes/' \
-        -e 's/^#*\(PermitEmptyPasswords\) .*/\1 yes/' \
-        -e 's/^#*\(UsePAM\) .*/\1 no/' \
-        /etc/ssh/sshd_config
-    service ssh restart;
-fi
-
-# If a git repo is specified in the environment variable, then replace existing d7 code.
-gitrepo="${GIT_REPO}";
-if [ "$gitrepo" != "" ]; 
-then 
-    CD ${SITEROOT}/../
-    echo "Pulling from repository ... "
-    git config --global --unset https.proxy && git config --global --unset http.proxy
-    git clone $(echo ${gitrepo}) moveme
-    # Allow for creating a new branch if specified in the configuration or docker run command.
-    if [ "$MAKE_GIT_BRANCH" != "" ]; 
-    then
-        gitbranchname=$MAKE_GIT_BRANCH
-        if [ "$MAKE_GIT_BRANCH" != "" ]; then gitbranchname="$MAKE_GIT_BRANCH_NAME"; fi
-        git checkout -b ${gitbranchname} || true
-        git push origin ${gitbranchname} || true;
-    fi
-    rm -r ${SITEROOT}/* ${SITEROOT}/.* 2> /dev/null
-    mv moveme/.* ${SITEROOT}/ 2> /dev/null
-    mv moveme/* ${SITEROOT}/ 2> /dev/null
-    rm -r moveme;
-    chown -R www-data:www-data ${SITEROOT}/sites
 fi
 
 # Additional commands can be added by an environment variable
