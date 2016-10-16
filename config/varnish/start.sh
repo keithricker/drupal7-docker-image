0eb5ca596449a7a1827b9e38e0964465c014fdad
Keith Ricker <keithricker@hotmail.com>
2016-10-16 10:01:14 -0500

    Update start.sh

--- config/varnish/start.sh
+++ config/varnish/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 if [ ! -z "$VARNISH_CONTENT_VCL" ]; then
 	echo -e "$VARNISH_CONTENT_VCL" > /etc/varnish/default.vcl
 	VARNISH_CONTENT="-f /etc/varnish/default.vcl"
 fi
-exec /usr/sbin/varnishd -a :$VARNISH_LISTEN_PORT $VARNISH_CONTENT -s $VARNISH_CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+
+exec /usr/sbin/varnishd -a :$VARNISH_LISTEN_PORT $VARNISH_CONTENT -s $VARNISH_CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+
+exit 0;
