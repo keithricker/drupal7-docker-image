5b3fcb001a2a2178d202d2c44bc0d1d5c9e2118f
Keith Ricker <keithricker@hotmail.com>
2016-10-16 12:02:54 -0500

    changing exit to return

--- config/varnish/start.sh
+++ config/varnish/start.sh
@@ -5,6 +5,6 @@ if [ ! -z "$VARNISH_CONTENT_VCL" ]; then
 	VARNISH_CONTENT="-f /etc/varnish/default.vcl"
 fi
 
-exec /usr/sbin/varnishd -a :$VARNISH_LISTEN_PORT $VARNISH_CONTENT -s $VARNISH_CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+exec /usr/sbin/varnishd -a :$VARNISH_LISTEN_PORT $VARNISH_CONTENT -s $VARNISH_CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
 
-exit 0;
+return;
