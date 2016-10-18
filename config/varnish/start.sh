33b26be58546753295d80477258fcf2e1b4848a6
Keith Ricker <keithricker@hotmail.com>
2016-10-17 19:32:27 -0500

    making this varnish thing easier

--- config/varnish/start.sh
+++ config/varnish/start.sh
@@ -5,9 +5,4 @@ if [ "${HOSTNAME}" != "" ]; then VARNISH_BACKEND_IP="${HOSTNAME}"; fi
 sed -i -e “s/\${VARNISH_BACKEND_IP}/${varnishbeip}/g” /etc/varnish/default.vcl
 sed -i -e “s/\${VARNISH_BACKEND_PORT}/${VARNISH_BACKEND_PORT}/g” /etc/varnish/default.vcl
 
-if [ ! -z "$VARNISH_CONTENT_VCL" ]; then
-    echo -e "$VARNISH_CONTENT_VCL" > /etc/varnish/default.vcl
-    VARNISH_CONTENT="-f /etc/varnish/default.vcl"
-fi
-
-exec /usr/sbin/varnishd -a :$VARNISH_LISTEN_PORT $VARNISH_CONTENT -s $VARNISH_CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+service varnish start
