6515c2f89dbaf4c3bf9856fac8d7b61e5c7bb657
Keith Ricker <keithricker@hotmail.com>
2016-10-19 11:12:59 -0500

    Update drupal-start.sh

--- config/drupal-start.sh
+++ config/drupal-start.sh
@@ -43,7 +43,8 @@ function replace_codebase {
     if [ -f "$1" ]
     then
         mv -f $1 ${CODEBASEDIR}/codebase.tar.gz || true
-        cd ${CODEBASEDIR} && tar -xz --strip-components=1 -f codebase.tar.gz && rm ${CODEBASEDIR}/codebase.tar.gz && cd ${SITEROOT}
+        cd ${CODEBASEDIR} && tar -xz --strip-components=1 -f --keep-newer-files codebase.tar.gz
+        rm codebase.tar.gz && cd ${SITEROOT}
     fi
     rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true
     rm -r ${CODEBASEDIR:?}/* ${CODEBASEDIR}/.* || true;
