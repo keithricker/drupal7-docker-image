ecc774ab2c8e25d13c623e7ebb99683eda15e864
Keith Ricker <keithricker@hotmail.com>
2016-10-18 20:51:03 -0500

    Update start.sh

--- config/varnish/start.sh
+++ config/varnish/start.sh
@@ -10,7 +10,7 @@ fi
 
 # Fix up the varnish config file because it doesn't seem to like variables.
 if [ "${HOSTNAME}" != "" ]; then VARNISH_BACKEND_IP="${HOSTNAME}"; fi
-sed -i -e “s/\${VARNISH_BACKEND_IP}/${varnishbeip}/g” /etc/varnish/default.vcl
-sed -i -e “s/\${VARNISH_BACKEND_PORT}/${VARNISH_BACKEND_PORT}/g” /etc/varnish/default.vcl
+sed -i -e “s/\${VARNISH_BACKEND_IP}/${varnishbeip}/g” /root/host_app/config/varnish/default.vcl
+sed -i -e “s/\${VARNISH_BACKEND_PORT}/${VARNISH_BACKEND_PORT}/g” /root/host_app/config/varnish/default.vcl
 
 service varnish start
