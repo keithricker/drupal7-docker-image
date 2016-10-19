052377ce16baf94ee7a62c91ed396ab5fd6bcc5c
Keith Ricker <keithricker@hotmail.com>
2016-10-19 01:21:50 -0500

    Update set_listen_port.sh

--- config/apache/set_listen_port.sh
+++ config/apache/set_listen_port.sh
@@ -4,3 +4,5 @@
 sed -i 's/Listen .*/Listen ${APACHE_LISTEN_PORT}/g' /etc/apache2/ports.conf
 sed -i 's/VirtualHost \*:.*/VirtualHost \*:${APACHE_LISTEN_PORT}/g' /etc/apache2/sites-available/default-ssl.conf
 sed -i 's/VirtualHost \*:.*/VirtualHost \*:${APACHE_LISTEN_PORT}/g' /etc/apache2/sites-available/000-default.conf
+
+service apache2 reload
