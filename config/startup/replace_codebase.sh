d10460d516c2b5db28390069fce44862f1e1b0fe
Keith Ricker <keithricker@hotmail.com>
2016-10-19 12:39:12 -0500

    Create replace_codebase.sh

---
+++ config/startup/replace_codebase.sh
@@ -0,0 +1,10 @@
+function replace_codebase {
+    if [ -f "$1" ]
+    then
+        mv -f $1 ${CODEBASEDIR}/codebase.tar.gz || true
+        cd ${CODEBASEDIR} && tar -xz --strip-components=1 -f --keep-newer-files codebase.tar.gz
+        rm codebase.tar.gz && cd ${SITEROOT}
+    fi
+    rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true
+    rm -r ${CODEBASEDIR:?}/* ${CODEBASEDIR}/.* || true;
+} 
