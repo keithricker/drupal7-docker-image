16d81387947808a28b0a52a83ea15266b556e6ab
Keith Ricker <keithricker@hotmail.com>
2016-10-19 21:13:30 -0500

    keeping permissions on the tarring of things

--- config/scripts/startup/replace_codebase.sh
+++ config/scripts/startup/replace_codebase.sh
@@ -5,7 +5,7 @@ function replace_codebase {
     if [ -f "$1" ]
     then
         mv -f $1 ${CODEBASEDIR}/codebase.tar.gz || true
-        cd ${CODEBASEDIR} && tar -xz --strip-components=1 -f --keep-newer-files -p codebase.tar.gz
+        cd ${CODEBASEDIR} && tar -p -xz --strip-components=1 --keep-newer-files -f codebase.tar.gz
         rm codebase.tar.gz && cd ${SITEROOT}
     fi
     rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true && chown -r www-data:www-data ${SITEROOT}
