e7d94fb6f516a6c7e38a77226e309b72d766ea50
Keith Ricker <keithricker@hotmail.com>
2016-10-19 21:50:23 -0500

    getting clever here

--- config/apache/set_listen_port.sh
+++ config/apache/set_listen_port.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify existing Apache2 configuration to give port 80 over to varnish
-sed -i "s/Listen 80 /Listen ${APACHE_LISTEN_PORT} /g" /etc/apache2/ports.conf
+sed -i "s/Listen 80\$/Listen ${APACHE_LISTEN_PORT}/g" /etc/apache2/ports.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/default-ssl.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/000-default.conf
 
