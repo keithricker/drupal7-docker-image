8ec98245603681806276584f512bb3e22b310621
Keith Ricker <keithricker@hotmail.com>
2016-10-21 10:33:45 -0500

    Update drupal-start.sh

--- config/scripts/startup/drupal-start.sh
+++ config/scripts/startup/drupal-start.sh
@@ -142,7 +142,43 @@ echo ""
 echo "Creating a new Drupal site at ${DRUPAL_SITE_DIR}/$dir"
 echo ""
 
-source ${startupscripts}/install_drupal.sh
+# If we're establishing a connection and we have data, then we'll assume we're installed and we'll move along.
+if drush pm-info node; then echo "Site is already installed here. Moving along." && continue; fi
+
+echo "Attempting to import the database."
+
+if [ "$dir" != "default" ]; then drush sql-create --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes || true; fi
+
+if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
+
+if ! drush site-install standard --site-name=${drupalsitename} --account-pass=$adminpass --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes
+then
+  echo "Unable to configure your Drupal installation at $DRUPAL_SITE_DIR/$dir"
+  echo ""
+  continue
+fi
+
+if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
+
+if [ "$dir" != "default" ]
+then
+  drush sql-create --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes || true
+fi
+
+if ! drush site-install minimal --site-name=${drupalsitename} --account-pass=$adminpass --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes
+then
+  echo "Unable to configure your Drupal installation at $DRUPAL_SITE_DIR/$dir"
+  echo
+  continue
+fi
+
+# If we're importing an external database, then we'll attempt to connect to the external server and grab it.
+if [ "${IMPORT_EXTERNAL_DB}" ]
+then
+   echo "Attempting to import the database."
+   source ${startupscripts}/import_external_db.sh
+fi
+
 echo "Just got done installing site ... "
 
 # Install backup and migrate
