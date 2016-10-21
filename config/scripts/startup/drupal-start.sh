ba4fa811fa4941957873745f5b2f0c64976a340c
Keith Ricker <keithricker@hotmail.com>
2016-10-21 10:40:10 -0500

    Update drupal-start.sh

--- config/scripts/startup/drupal-start.sh
+++ config/scripts/startup/drupal-start.sh
@@ -151,20 +151,6 @@ if [ "$dir" != "default" ]; then drush sql-create --db-url=mysql://$dbuname:$dbp
 
 if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
 
-if ! drush site-install standard --site-name=${drupalsitename} --account-pass=$adminpass --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes
-then
-  echo "Unable to configure your Drupal installation at $DRUPAL_SITE_DIR/$dir"
-  echo ""
-  continue
-fi
-
-if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
-
-if [ "$dir" != "default" ]
-then
-  drush sql-create --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes || true
-fi
-
 if ! drush site-install minimal --site-name=${drupalsitename} --account-pass=$adminpass --db-url=mysql://$dbuname:$dbpass@$dbhost:$dbport/$dbname --yes
 then
   echo "Unable to configure your Drupal installation at $DRUPAL_SITE_DIR/$dir"
