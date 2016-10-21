aefe1c236b99785dab3a5e0b95de5868e10670c2
Keith Ricker <keithricker@hotmail.com>
2016-10-21 02:53:10 -0500

    Update install_drupal.sh

--- config/scripts/startup/install_drupal.sh
+++ config/scripts/startup/install_drupal.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# If we're establishing a connection and we have data, then we'll assume we're installed and we'll move along.
+if drush pm-info node; then echo "Site is already installed here. Moving along." && continue; fi
+
+# If we're importing an external database, then we'll attempt to connect to the external server and grab it.
+if [ "${IMPORT_EXTERNAL_DB}" ]
+then
+   echo "Attempting to import the database."
+   remotecommand="cd ${EXTERNAL_DB_WEBROOT}/sites/${dir} && "
+   remotecommand+="drush cc all && drush sql-dump -y > ~/my-sql-dump-file.sql"
+   ssh -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP} "${remotecommand}"
+   scp -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP}:~/my-sql-dump-file.sql /root
+fi
+
 if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
 
 if [ "$dir" != "default" ]
