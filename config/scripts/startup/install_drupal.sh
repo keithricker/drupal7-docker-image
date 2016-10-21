9692caa74714765270ac819091b8068e49604ade
Keith Ricker <keithricker@hotmail.com>
2016-10-21 10:06:07 -0500

    Update install_drupal.sh

--- config/scripts/startup/install_drupal.sh
+++ config/scripts/startup/install_drupal.sh
@@ -3,16 +3,13 @@
 # If we're establishing a connection and we have data, then we'll assume we're installed and we'll move along.
 if drush pm-info node; then echo "Site is already installed here. Moving along." && continue; fi
 
-# If we're importing an external database, then we'll attempt to connect to the external server and grab it.
-if [ "${IMPORT_EXTERNAL_DB}" ]
-then
-   echo "Attempting to import the database."
-   remotecommand="cd ${EXTERNAL_DB_WEBROOT}/sites/${dir} && "
-   remotecommand+="drush cc all && drush sql-dump -y > ~/my-sql-dump-file.sql"
-   ssh -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP} "${remotecommand}"
-   scp -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP}:~/my-sql-dump-file.sql /root
-   drush sql-cli < /root/my-sql-dump-file.sql
-fi
+echo "Attempting to import the database."
+
+remotecommand = "ls -1rth ${EXTERNAL_DB_SITEROOT}/sites/default/files/private/backup_migrate/scheduled/*.mysql.gz | tail -1 ")"   
+LATEST_FILE=$(ssh -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP} "${remotecommand}")
+scp -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP}:${LATEST_FILE} ~/mysql-dump-file.sql
+
+if drush sql-cli < ~/my-sql-dump-file.sql; then echo "Database import successful" && continue; else echo "Database import unseccessful"; fi
 
 if [ -f "${DRUPAL_SETTINGS}" ]; then mv ${DRUPAL_SETTINGS} ${DRUPAL_SETTINGS}.bak; fi
 
