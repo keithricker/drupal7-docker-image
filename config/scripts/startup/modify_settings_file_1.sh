8b894140bce5bf0295b97fd70671558442979fa4
Keith Ricker <keithricker@hotmail.com>
2016-10-20 22:30:32 -0500

    Create modify_settings_file_1.sh

---
+++ config/scripts/startup/modify_settings_file_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -d '' output <<- EOF
+#
+# Add include statement for local.settings.php
+#
+$drupalenv = getenv('DRUPAL_ENVIRONMENT');
+if (empty($drupalenv)) $drupalenv = "local";
+$localsettings = $drupalenv.'.settings.php';
+if (is_readable(dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings)) {
+    include dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings;
+}  
+EOF 
+echo "$output" >> ${DRUPAL_SETTINGS}
