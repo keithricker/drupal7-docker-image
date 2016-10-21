7b56a3a1697cec8fac7ba9827f2a904e3304b872
Keith Ricker <keithricker@hotmail.com>
2016-10-21 11:24:28 -0500

    Update copy_private_key.sh

--- config/scripts/startup/copy_private_key.sh
+++ config/scripts/startup/copy_private_key.sh
@@ -3,10 +3,9 @@
 if [ "${PRIVATE_KEY_CONTENTS}" != "" ]
 then
     echo "Copying over the private key."
-    echo "${PRIVATE_KEY_CONTENTS}" > ~/.ssh/${PRIVATE_KEY_FILE}
+    echo "${PRIVATE_KEY_CONTENTS}" > ~/.ssh_copy/${PRIVATE_KEY_FILE}
     sed -i 's/\\n/\
-/g' ~/.ssh/${PRIVATE_KEY_FILE}
-    chmod 600  ~/.ssh/${PRIVATE_KEY_FILE}
-else
-    cp -a ~/.ssh_copy/. ~/.ssh/ && chown -R root:root ~/.ssh ~/.ssh/*
+/g' ~/.ssh_copy/${PRIVATE_KEY_FILE}
+    chmod 600  ~/.ssh_copy/${PRIVATE_KEY_FILE}
 fi
+cp -a ~/.ssh_copy/. ~/.ssh/ && chown -R root:root ~/.ssh ~/.ssh/*
