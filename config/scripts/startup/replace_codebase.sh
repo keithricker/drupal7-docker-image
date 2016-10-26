0c1a0954a21e7d30e41fcbfad6e29d22a39f007a
Keith Ricker <keithricker@hotmail.com>
2016-10-26 09:14:02 -0500

    Update replace_codebase.sh

--- config/scripts/startup/replace_codebase.sh
+++ config/scripts/startup/replace_codebase.sh
@@ -8,6 +8,6 @@ function replace_codebase {
         cd ${CODEBASEDIR} && tar -p -xz --strip-components=1 --keep-newer-files -f codebase.tar.gz
         rm codebase.tar.gz && cd ${SITEROOT}
     fi
-    rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true && chown -r www-data:www-data ${SITEROOT}
+    rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true && chown -R www-data:www-data ${SITEROOT}
     rm -rf ${CODEBASEDIR:?}/* ${CODEBASEDIR}/.* || true;
 } 
