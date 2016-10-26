becb7fccf36b6d1e9a37891a7d063f8c3552b51e
Keith Ricker <keithricker@hotmail.com>
2016-10-26 15:38:06 -0500

    Updating variable definitions to be less verbose

--- config/scripts/startup/drupal_config_variables.sh
+++ config/scripts/startup/drupal_config_variables.sh
@@ -2,20 +2,20 @@
 
 # Define a bunch of variables we will use for configuring our site installation. Database credentials and so forth.
 
-drupalprofile=minimal
-drupalsitename=drupal7
-adminpass=drupal
-dbname=drupal
-dbhost=database
-dbuname=drupal
-dbpass=drupal
-dbport=3306
-# We'll over-ride the defaults if environment variables are defining them.
-if [ "${DRUPAL_SITENAME}" != "" ]; then drupalsitename="${DRUPAL_SITENAME}"; fi
-if [ "${MYSQL_ENV_MYSQL_DATABASE}" != "" ]; then dbname=$MYSQL_ENV_MYSQL_DATABASE; fi
-if [ "${MYSQL_ENV_MYSQL_USER}" != "" ]; then dbuname=$MYSQL_ENV_MYSQL_USER; fi
-if [ "${MYSQL_ENV_MYSQL_PASSWORD}" != "" ]; then dbpass=$MYSQL_ENV_MYSQL_PASSWORD; fi
-if [ "${MYSQL_PORT_3306_TCP_PORT}" != "" ]; then dbport=$MYSQL_PORT_3306_TCP_PORT; fi
+drupalprofile=${DRUPAL_PROFILE:-minimal}
+drupalsitename=${HOSTNAME:-drupal7}
+drupalsitename=${DRUPAL_SITENAME:-$drupalsitename}
+
+# If framework variable is set then we'll use this for just about everything.
+FRAMEWORK=${FRAMEWORK:-drupal}
+adminpass=${FRAMEWORK}
+dbname=${FRAMEWORK}
+
+dbhost=${DB_LINK:-database}
+# dbhost=${MYSQL_PORT_3306_TCP_ADDR:-database}
+dbuname=${MYSQL_ENV_MYSQL_USER:-$FRAMEWORK}
+dbpass=${MYSQL_ENV_MYSQL_PASSWORD:-$FRAMEWORK}
+dbport=${MYSQL_PORT_3306_TCP_PORT:-3306}
 
 DRUPAL_SITE_DIR=${SITEROOT}/sites
 DRUPAL_FILES_DIR=${SITEROOT}/sites/default/files
