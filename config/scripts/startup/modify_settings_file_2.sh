f556511f9b35c853f85114660a3b587e94fb1e78
Keith Ricker <keithricker@hotmail.com>
2016-10-26 01:30:30 -0500

    Update modify_settings_file_2.sh

--- config/scripts/startup/modify_settings_file_2.sh
+++ config/scripts/startup/modify_settings_file_2.sh
@@ -4,11 +4,11 @@
 # Set a base_url with correct scheme, based on the current request. This is
 # used for internal links to stylesheets and javascript.
 #
-echo "\$scheme = !empty(\$_SERVER['REQUEST_SCHEME']) ? \$_SERVER['REQUEST_SCHEME'] : 'http';" >> ${DRUPAL_SETTINGS}
-echo "\$base_url = \$scheme . '://' . \$_SERVER['HTTP_HOST'];" >> ${DRUPAL_SETTINGS}
+echo "\$scheme = !empty(\$_SERVER['REQUEST_SCHEME']) ? \$_SERVER['REQUEST_SCHEME'] : 'http';" >> ${DRUPAL_LOCAL_SETTINGS}
+echo "\$base_url = \$scheme . '://' . \$_SERVER['HTTP_HOST'];" >> ${DRUPAL_LOCAL_SETTINGS}
 
 #
 # Set the private and temp directories
 #
-echo "\$conf['file_private_path'] = '$DRUPAL_PRIVATE_DIR';" >> ${DRUPAL_SETTINGS}
-echo "\$conf['file_temporary_path'] = '$DRUPAL_TMP_DIR';" >> ${DRUPAL_SETTINGS}
+echo "\$conf['file_private_path'] = '$DRUPAL_PRIVATE_DIR';" >> ${DRUPAL_LOCAL_SETTINGS}
+echo "\$conf['file_temporary_path'] = '$DRUPAL_TMP_DIR';" >> ${DRUPAL_LOCAL_SETTINGS}
