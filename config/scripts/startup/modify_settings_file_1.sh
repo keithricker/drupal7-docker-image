9bf1b06c53eab9f17bc66c63a698919ff13570f7
Keith Ricker <keithricker@hotmail.com>
2016-10-26 19:49:15 -0500

    Update modify_settings_file_1.sh

--- config/scripts/startup/modify_settings_file_1.sh
+++ config/scripts/startup/modify_settings_file_1.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-addthis="\n \
-#\n \
-# Add include statement for local.settings.php\n\
-#\n\
-\$drupalenv = getenv('DRUPAL_ENVIRONMENT'); \n\
-if (empty(\$drupalenv)) \$drupalenv = \"local\"; \n\
-\$localsettings = \$drupalenv.'.settings.php'; \n\
-if (is_readable(dirname(__FILE__) . DIRECTORY_SEPARATOR . \$localsettings)) { \n\
-    include dirname(__FILE__) . DIRECTORY_SEPARATOR . \$localsettings; \n\
-}"
-echo "${addthis}" >> ${DRUPAL_SETTINGS} && chown www-data:www-data ${DRUPAL_SETTINGS}
+export add_this_to_settings=$(cat <<END
+#
+# Add include statement for local.settings.php
+#
+$drupalenv = getenv('DRUPAL_ENVIRONMENT');
+if (empty($drupalenv)) $drupalenv = "local";
+$localsettings = $drupalenv.'.settings.php';
+if (is_readable(dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings)) {
+    include dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings;
+}
+
+END
+); 
+
+echo "${add_this_to_settings}" >> ${DRUPAL_SETTINGS} && chown www-data:www-data ${DRUPAL_SETTINGS}
