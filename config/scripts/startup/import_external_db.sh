bf47dcef4f22f27ea243d10077390640b0d2ae4e
Keith Ricker <keithricker@hotmail.com>
2016-10-28 11:49:18 -0500

    Update import_external_db.sh

--- config/scripts/startup/import_external_db.sh
+++ config/scripts/startup/import_external_db.sh
@@ -5,7 +5,7 @@
 # but unfortunately I'm not much of a bash expert.
 
 # Navigate to the corresponding "site" directory
-remotecommand = "cd ${EXTERNAL_DB_SITEROOT}/sites/$dir && "
+remotecommand = "cd ${EXTERNAL_DB_SRC_SITEROOT}/sites/$dir && "
 # 1. Start by calling drush bam-backups to get a list of backup files and grab just the first line of those results.
 remotecommand+="firstline=\$(drush bam-backups | sed -n '2p' | xargs) &&"
 # 2. Get just the file name from the first line of results
@@ -13,8 +13,8 @@ remotecommand+='set -- $firstline && backupfile=$1 && '
 # 3. Get the full, absolute path to that file
 remotecommand+='backupfile=$(readlink -f $(find files/private/backup_migrate -name ${backupfile})) && echo $backupfile'
    
-LATEST_FILE=$(ssh -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP} "${remotecommand}")
-scp -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_IP}:${LATEST_FILE} ~/mysql-dump-file.sql
+LATEST_FILE=$(ssh -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_SRC_IP} "${remotecommand}")
+scp -i ~/.ssh/${PRIVATE_KEY_FILE} ${EXTERNAL_DB_USER}@${EXTERNAL_DB_SRC_IP}:${LATEST_FILE} ~/mysql-dump-file.sql
 
 if drush sql-cli < ~/my-sql-dump-file.sql; 
 then 
