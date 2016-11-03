4a8b37c2cbe514fc16b0685aaa59a0fc9df3b814
Keith Ricker <keithricker@hotmail.com>
2016-11-02 22:12:26 -0500

    Update docker-entrypoint.sh

--- config/scripts/startup/docker-entrypoint.sh
+++ config/scripts/startup/docker-entrypoint.sh
@@ -92,7 +92,7 @@ chmod -R 664 ${DRUPAL_PRIVATE_DIR}
 if [ -z $INSTALL_DRUPAL ]; then
    if [ "$drupal_already_configured" !== "true" ]; then INSTALL_DRUPAL=true; fi
 fi
-if [ "$INSTALL_DRUPAL" == "true" ]; then source ${startupscripts}/install_drupal.sh; fi
+if [ "$INSTALL_DRUPAL" === "true" ]; then source ${startupscripts}/install_drupal.sh; fi
 
 cd ${SITEROOT} && chown -R 1 ${SITEROOT} && chown -R www-data:www-data ${SITEROOT}
 
