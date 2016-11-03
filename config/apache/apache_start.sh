c496052e8d42ad685d7c9a3702321b01eafaf0df
Keith Ricker <keithricker@hotmail.com>
2016-11-03 13:39:59 -0500

    Update apache_start.sh

--- config/apache/apache_start.sh
+++ config/apache/apache_start.sh
@@ -5,4 +5,4 @@ sed -i "s/Listen 80\$/Listen ${APACHE_LISTEN_PORT}/g" /etc/apache2/ports.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/default-ssl.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/000-default.conf
 
-apache2-foreground
+apache2-foreground || service apache2 start || true
