539d6d77502083ee12d0ed8c6bcde8bf839a60a6
Keith Ricker <keithricker@hotmail.com>
2016-11-03 00:40:38 -0500

    Update and rename set_listen_port.sh to apache_start.sh

--- config/apache/apache_start.sh
+++ config/apache/apache_start.sh
@@ -4,3 +4,5 @@
 sed -i "s/Listen 80\$/Listen ${APACHE_LISTEN_PORT}/g" /etc/apache2/ports.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/default-ssl.conf
 sed -i "s/VirtualHost \*:80>/VirtualHost \*:${APACHE_LISTEN_PORT}>/g" /etc/apache2/sites-available/000-default.conf
+
+apache2-foreground
