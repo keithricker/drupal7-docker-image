0b35f248e3813828e803defabf433d717480fcd4
Keith Ricker <keithricker@hotmail.com>
2016-11-03 16:22:09 -0500

    Update drupal_config_variables.sh

--- config/scripts/drupal_config_variables.sh
+++ config/scripts/drupal_config_variables.sh
@@ -2,8 +2,9 @@
 set -a
 
 # Define a bunch of variables we will use for configuring our site installation. Database credentials and so forth.
-ROOT_USER_ID=${ROOT_USER_ID:-1}
-if [ ! -z $ROOT_GROUP_ID ]; then ownership=${ROOT_USER_ID}:${ROOT_GROUP_ID}; else ownershp=$ROOT_USER_ID; fi
+ROOT_USER_ID=${ROOT_USER_ID:-"1"}
+ROOT_GROUP_ID=${ROOT_GROUP_ID:-"0"}
+export ownershp="${ROOT_USER_ID}:${ROOT_GROUP_ID}"
 
 hostconfig=/host_app/config
 drupalscripts=/host_app/config/drupal/scripts
