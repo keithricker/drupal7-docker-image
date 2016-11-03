4664ecd7344c0a4c99724f1ca89642a0632cd3cb
Keith Ricker <keithricker@hotmail.com>
2016-11-02 22:29:05 -0500

    Update docker-entrypoint.sh

--- config/scripts/startup/docker-entrypoint.sh
+++ config/scripts/startup/docker-entrypoint.sh
@@ -43,7 +43,7 @@ if [ -f "${SITEROOT}/sites/default/local.settings.php" ]; then drupal_already_co
 if [ "${GIT_REPO}" != "" ]; then git_repo_exists=true; fi
 
 # Yes this is more code than necessary but it makes things esier to follow along with.
-if [ "$drupal_already_configured" == "true" ] && [ ! "$git_repo_exists" ]; then move_along=true; fi
+if [ "$drupal_already_configured" == "true" ] && [ ! "$git_repo_exists" ] && [ "$INSTALL_DRUPAL" != "true" ]; then move_along=true; fi
 if [ "$drupal_files_exist" ] && [ "$git_repo_exists" ]; then pull_from_git=true; fi
 if [ ! "$drupal_files_exist" ] && [ "$git_repo_exists" ]; then clone_from_git=true; fi
 if [ ! "$drupal_files_exist" ] && [ ! "$git_repo_exists" ]; then install_drupal_from_scratch=true; fi
