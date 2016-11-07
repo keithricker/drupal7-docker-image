245a9df73be1459c367e5bc21f2f2d8691b05df8
Keith Ricker <keithricker@hotmail.com>
2016-11-07 11:53:21 -0600

    Create modify_settings_file_1.sh

---
+++ config/drush/bin/modify_settings_file_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -a
+
+export add_this_to_settings=$(cat <<END
+/*
+ * Add include statement for local.settings.php
+ */
+$drupalenv = getenv('DRUPAL_ENVIRONMENT');
+if (empty($drupalenv)) $drupalenv = "local";
+$localsettings = $drupalenv.'.settings.php';
+if (is_readable(dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings)) {
+    include dirname(__FILE__) . DIRECTORY_SEPARATOR . $localsettings;
+}
+END
+); 
+
+echo "${add_this_to_settings}" >> ${DRUPAL_SETTINGS} && chown www-data:www-data ${DRUPAL_SETTINGS}
