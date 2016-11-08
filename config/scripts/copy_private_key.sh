1deaeb84192facc2d7997b8ed3b24e110d0c8663
Keith Ricker <keithricker@hotmail.com>
2016-11-08 14:18:55 -0600

    Update copy_private_key.sh

--- config/scripts/copy_private_key.sh
+++ config/scripts/copy_private_key.sh
@@ -6,6 +6,6 @@ then
     echo "${PRIVATE_KEY_CONTENTS}" > ~/.ssh_copy/${PRIVATE_KEY_FILE}
     sed -i 's/\\n/\
 /g' ~/.ssh_copy/${PRIVATE_KEY_FILE}
-    chmod 600  ~/.ssh_copy/${PRIVATE_KEY_FILE}
+    chmod 600  ~/.ssh_copy/${PRIVATE_KEY_FILE} || true
 fi
 rsync -a ~/.ssh_copy/ /root/.ssh || true && chown -R root:root /root/.ssh || true
