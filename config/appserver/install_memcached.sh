2770644dc5e25ccdaa053e935f8629390ad57e7b
Keith Ricker <keithricker@hotmail.com>
2016-11-07 20:27:22 -0600

    Update install_memcached.sh

--- config/appserver/install_memcached.sh
+++ config/appserver/install_memcached.sh
@@ -4,4 +4,5 @@ set -a
 apt-get update && apt-get install -y memcached libmemcached-dev libmemcached11 git build-essential || true
 export PHP_EXT_DIR /usr/src/php/ext
 git clone -b php7 https://github.com/php-memcached-dev/php-memcached /usr/src/php/ext/memcached
-docker-php-ext-install memcached
+docker-php-ext-install memcached || true
+service start memcached || true
