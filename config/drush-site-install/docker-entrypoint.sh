89c07916bf2121e5f3c487ad1e83f24009c62055
Keith Ricker <keithricker@hotmail.com>
2016-11-08 16:15:53 -0600

    Update docker-entrypoint.sh

--- config/drush-site-install/docker-entrypoint.sh
+++ config/drush-site-install/docker-entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -a
 
-"$(printenv)[@]"|while read line; do 
-    modline=export $(sed "s/APPSERVER_//g" <<< $line) | xargs
-    statement="export $modline"
-    eval ${statement}
-done
-
 source /host_app/config/drush-site-install/install_drupal.sh
