8840a14d6091106cd1c642d81c0cbddcb9f59ea3
Keith Ricker <keithricker@hotmail.com>
2016-11-08 13:37:12 -0600

    Update docker-entrypoint.sh

--- config/appserver/docker-entrypoint.sh
+++ config/appserver/docker-entrypoint.sh
@@ -8,11 +8,9 @@ source ${drupalscripts}/drupal_config_variables.sh
 
 # Copy shared files from server container to docker host machine for sharing
 # Move anything newer from the container to the host, and delete anything in the existing config folder.
-if [ ! -d "/host_app/config/drupal" ]; then
-   if [ ! -d "/root/config" ]; then 
-      echo "Container is not configured properly. Missing configuration directory."
-      exit 0
-   fi
+if [ ! -d "/host_app/config/appserver" ]; then
+   echo "Container is not configured properly. Missing configuration directory."
+   exit 0
 fi
 
 if [ ! -f "${SITEROOT}/index.php" ] && [ -f "${CODEBASEDIR}/index.php" ]; then
