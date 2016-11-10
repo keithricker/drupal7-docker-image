59da980977ad14c84e3a4314a588c0f505609d77
Keith Ricker <keithricker@hotmail.com>
2016-11-10 12:46:20 -0600

    Create docker-entrypoint.sh

---
+++ config/drush-site-install/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get update -y || true
+apt-get install inotify-tools -y
+
+while true
+do 
+inotifywait -r -e modify,close_write,move,create,delete ${SITEROOT}/sites/default && install_drupal
+done
