a1324f64679dafe19e3d43fefb8e6f2704b335b8
Keith Ricker <keithricker@hotmail.com>
2016-11-10 16:50:39 -0600

    Update docker-entrypoint.sh

--- config/drush-site-install/docker-entrypoint.sh
+++ config/drush-site-install/docker-entrypoint.sh
@@ -5,6 +5,6 @@ apt-get update -y || true
 apt-get install inotify-tools -y
 
 while true
-do 
-inotifywait -e modify,close_write,move,create,delete ${SITEROOT}/sites && install_drupal
+do
+inotifywait -e modify,move,create ${SITEROOT}/sites && install_drupal
 done
