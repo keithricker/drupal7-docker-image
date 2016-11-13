0dd901cc6c7ce80ce80e5e773172a4e3ef7ebf59
Keith Ricker <keithricker@hotmail.com>
2016-11-13 12:39:57 -0600

    Update replace_codebase.sh

--- config/scripts/replace_codebase.sh
+++ config/scripts/replace_codebase.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -a
 
-if [ ! -d "${CODEBASEDIR}" ]; then mkdir -p ${CODEBASEDIR}; fi
-
 function replace_codebase {
-    if [ -f "$1" ]
-    then
-        mv -f $1 ${CODEBASEDIR}/codebase.tar.gz || true
-        cd ${CODEBASEDIR} && tar -p -xz --strip-components=1 --keep-newer-files -f codebase.tar.gz
-        rm codebase.tar.gz && cd ${SITEROOT}
+
+    # By default we'll assume the sync-from directory is "Codebase," unless specified as an argument to the function.
+    local syncfrom="${CODEBASEDIR}";
+    
+    if [ ! -d "$1" ]; then
+       if [ ! -d "${CODEBASEDIR}" ]; then mkdir -p ${CODEBASEDIR} || true; fi
+    else
+       syncfrom="$1";
     fi
-    rsync -a ${CODEBASEDIR}/ ${SITEROOT}/ || true
-    rm -rf ${CODEBASEDIR:?}/* ${CODEBASEDIR}/.* || true;
+    
+    if [ -f "$1" ]; then
+       mv -f $1 ${syncfrom}/codebase.tar.gz || true
+       cd ${syncfrom} && tar -p -xz --strip-components=1 --keep-newer-files -f codebase.tar.gz || true
+       rm codebase.tar.gz || true && cd ${SITEROOT}
+    fi
+
+    rsync -a ${syncfrom}/ ${SITEROOT}/ || true
+    rm -rf ${syncfrom:?}/* ${syncfrom}/.* || true;
 } 
